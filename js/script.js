ca02fb788cd191e42a5c40935bf4dd927e321ed2
Fadel <mfb.bioinfo@gmail.com>
2016-05-16 18:16:49 +0800

    First commit

---
+++ js/script.js
@@ -0,0 +1,89 @@
+/**
+ * Created by Phoenix on 5/16/16.
+ */
+$(document).ready(function() {
+    $('#contact_form').bootstrapValidator({
+            // To use feedback icons, ensure that you use Bootstrap v3.1.0 or later
+            feedbackIcons: {
+                valid: 'fa fa-check',
+                invalid: 'fa-times',
+                validating: 'fa fa-refresh'
+            },
+            fields: {
+                first_name: {
+                    validators: {
+                        stringLength: {
+                            min: 2,
+                        },
+                        notEmpty: {
+                            message: 'Please supply your first name'
+                        }
+                    }
+                },
+                last_name: {
+                    validators: {
+                        stringLength: {
+                            min: 2,
+                        },
+                        notEmpty: {
+                            message: 'Please supply your last name'
+                        }
+                    }
+                },
+                email: {
+                    validators: {
+                        notEmpty: {
+                            message: 'Please supply your email address'
+                        },
+                        emailAddress: {
+                            message: 'Please supply a valid email address'
+                        }
+                    }
+                },
+
+
+                comment: {
+                    validators: {
+                        stringLength: {
+                            min: 10,
+                            max: 200,
+                            message:'Please enter at least 10 characters and no more than 200'
+                        },
+                        notEmpty: {
+                            message: 'Please supply a description of your project'
+                        }
+                    }
+                }
+            }
+        })
+        .on('success.form.bv', function(e) {
+            $('#success_message').slideDown({ opacity: "show" }, "slow")
+            $.ajax({
+                url: "https://formspree.io/graph4bio@gmail.com",
+                method: "POST",
+                data: {Name: $('#contactFName').val() + " " + $('#contactLName').val(),
+                    Email:$('#contactEmail').val(),
+                    Comment: $('#contactCom').val()},
+                dataType: "json"
+            });
+
+            // Do something ...
+            $('#contact_form').data('bootstrapValidator').resetForm();
+
+            // Prevent form submission
+            e.preventDefault();
+
+            // Get the form instance
+            var $form = $(e.target);
+
+            // Get the BootstrapValidator instance
+            var bv = $form.data('bootstrapValidator');
+
+            // Use Ajax to submit form data
+            $.post($form.attr('action'), $form.serialize(), function(result) {
+                console.log(result);
+            }, 'json');
+        });
+});
+
+
